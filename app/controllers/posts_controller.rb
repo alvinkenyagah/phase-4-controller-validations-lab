c9caac92341b5ed709ba957cf6dbed91ef2b4ea4
alvinkenyagah <alvin47k@gmail.com>
2023-06-24 18:07:23 +0300

    Completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+rescue_from ActiveRecord::RecordInvalid, with:  :render_unprocessable_entity
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+
+
   def show
     post = Post.find(params[:id])
     
@@ -9,12 +13,29 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
+
+  def create
+    post = Post.create!(post_params)
+
+    render json: post, status: :created
+  end
+
   private
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+  
+  
+
+# def render_not_found_response
+#   render json: { error: "Post not found" }, status: :not_found
+# end
 
   def post_params
     params.permit(:category, :content, :title)
